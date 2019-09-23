3033d412a1516bcd43e25dbbeca91bcbfb3b452b
Adriel <adrielgoulartdeabranjo@gmail.com>
2019-09-23 11:39:34 -0300

    Código para separar a matrizA

--- MultiplicacaoCliente/src/br/edu/ifsc/calculadoramatriz/CalculadoraClient.java
+++ MultiplicacaoCliente/src/br/edu/ifsc/calculadoramatriz/CalculadoraClient.java
@@ -5,25 +5,48 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
+import br.edu.ifsc.calculadoramatriz.carregamatrizes.CarregaMatriz;
 import br.edu.ifsc.calculadoramatriz.interfaces.ICalculadoraClient;
-
 
 public class CalculadoraClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("\nIniciando cliente da CalculadoraRemota...");
 
-		try {
-			ICalculadoraClient calc;
+		int lin = 2, col = 2;
 
+		CarregaMatriz ca = new CarregaMatriz();
+		// Carregando matriz A
+		String caminhoA = "src/matA.txt";
+		long[][] matA = ca.carregarMatriz(lin, col, caminhoA);
+		
+		//Carregando matriz B
+		String caminhoB = "src/matB.txt";
+		long[][] matB = ca.carregarMatriz(lin, col, caminhoB);
+		
+		// Separando dados da matriz A
+		long[] result = new long[col];
+		result[0] = matA[0][0];
+		result[1] = matA[0][1];
+			
+	
+		try {
+			ICalculadoraClient calcPc1;
+			
 			// Registra o gerenciador de segurança
-			System.out.println("\n\tRegistrando o gerenciador de segurança...");
+			System.out.println("\n\tRegistrando o gerenciador de seguran�a..");
 			System.setSecurityManager(new SecurityManager());
 
-			calc = (ICalculadoraClient)Naming.lookup("rmi://localhost:1099/CalculadoraRemota");
-
-			//System.out.println("\n\tMULT 2 + 2 =>" + calc.mult(2, 2));
+			calcPc1 = (ICalculadoraClient) Naming.lookup("rmi://localhost:1099/MultiplicacaoMatriz");
+			
+		    long[][] resultado = calcPc1.multiplicacaoMatriz(result, matB);
+		    
+		    for (int i = 0; i < resultado.length; i++) {
+				for (int j = 0; j < resultado.length; j++) {
+					System.out.println(resultado[i][j]);
+				}
+			}
 
 		} catch (MalformedURLException | RemoteException | NotBoundException e) {
 			System.err.print("\n\tErro: " + e.getMessage());
